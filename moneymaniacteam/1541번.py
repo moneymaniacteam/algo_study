3bc3cf76f4b9e1a5015d84320bf51c33a70e7c08
chan0139 <chan4756@gmail.com>
2021-05-20 00:36:09 +0900

    10844 / 5.19 / greedy

---
+++ moneymaniacteam/1541번.py
@@ -0,0 +1,20 @@
+
+str = input().split('-')
+result = []
+
+
+for i in range(len(str)):
+    if not str[i].isalnum():
+        temp = str[i].split("+")
+        temp = list(map(int, temp))
+        result.append(sum(temp))
+
+    else:
+        str[i] = int(str[i])
+        result.append(str[i])
+
+hap = result[0]
+
+for i in range(1, len(result)):
+    hap -= result[i]
+print(hap)
